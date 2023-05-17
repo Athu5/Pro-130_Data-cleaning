64bc844c0d64cc147d8287538b0d3415f47fb04d
Athu5 <84505685+Athu5@users.noreply.github.com>
2023-05-17 14:05:00 +0530

    Add files via upload

---
+++ Data_cleaning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+
+df = pd.read_csv("Data_merged.csv")
+
+del df["Luminosity"]
+del df["Unnamed: 0"]
+del df["Unnamed: 6"]
+del df["Star_name.1"]
+del df["Distance.1"]
+del df["Mass.1"]
+del df["Radius.1"]
+
+df.to_csv("final_data.csv")
